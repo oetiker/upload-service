18362893cbfece8d5009b031ded36509836c4491
Tobias Oetiker <tobi@oetiker.ch>
2013-10-21 09:18:42 +0200

    add secret

--- backend/bin/test-run.sh
+++ backend/bin/test-run.sh
@@ -2,5 +2,6 @@
 export US_ROOT=/tmp
 export US_ENABLE_DOWNLOAD=0
 export US_ENABLE_DELETE=0
+export US_SECRET_FILE=/root/upload-service-secret
 mkdir -p $US_ROOT/$USER/INBOX
 `dirname $0`/us.pl daemon --listen='http://*:9873'
